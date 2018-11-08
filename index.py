e185b7ecdcbc4d1d760d201568e08a7de9d91da7
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2018-11-09 00:09:50 +0700

    done basic face recognition + mqtt to esp

---
+++ index.py
@@ -0,0 +1,26 @@
+import face_recognition
+import paho.mqtt.client as paho
+
+mqttClient = paho.Client()
+
+host = "192.168.0.103"
+port = 3000
+
+def on_connect(mosq, obj, rc):
+   print("on_connect() "+str(rc))
+
+mqttClient.on_connect = on_connect
+
+print("Connecting to " + host)
+mqttClient.connect(host, port, 60)
+
+
+def main():
+    recognized = face_recognition.main()
+    print(recognized)
+    if recognized != "unknown":
+       mqttClient.publish("toEsp/control/device/3", "on")
+       print("Done!")
+
+main()
+
